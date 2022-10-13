1a5a08ce9b8ec0705b27333c62b316e2154a51cb
1729372667@qq.com <1729372667@qq.com>
2022-10-13 14:12:21 +0000

    detect_landmark multithreading, which draws speed down even more.

--- avhubert/preparation/detect_landmark.py
+++ avhubert/preparation/detect_landmark.py
@@ -14,9 +14,7 @@ def load_video(path):
     frames = np.array([frame for frame in videogen])
     return frames
 
-def detect_face_landmarks(face_predictor_path, cnn_detector_path, root_dir, landmark_dir, flist_fn, rank, nshard):
-
-    def detect_landmark(image, detector, cnn_detector, predictor):
+def detect_landmark(image, detector, cnn_detector, predictor):
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         rects = detector(gray, 1)
         if len(rects) == 0:
@@ -30,17 +28,37 @@ def detect_face_landmarks(face_predictor_path, cnn_detector_path, root_dir, land
                 coords[i] = (shape.part(i).x, shape.part(i).y)
         return coords
 
-    detector = dlib.get_frontal_face_detector()
-    cnn_detector = dlib.cnn_face_detection_model_v1(cnn_detector_path)
-    predictor = dlib.shape_predictor(face_predictor_path)
+def detect_face_landmarks(face_predictor_path, cnn_detector_path, root_dir, landmark_dir, flist_fn, nshard):
+    from concurrent.futures import ThreadPoolExecutor
+    def callback(future):
+        finished[future.result()] = 1
+        print(f'currently finished:{sum(finished)}')
+    pool = ThreadPoolExecutor(max_workers=64)
     input_dir = root_dir #
     output_dir = landmark_dir #
     fids = [ln.strip() for ln in open(flist_fn).readlines()]
     num_per_shard = math.ceil(len(fids)/nshard)
-    start_id, end_id = num_per_shard*rank, num_per_shard*(rank+1)
-    fids = fids[start_id: end_id]
-    print(f"{len(fids)} files")
-    for fid in tqdm(fids):
+    finished = [0] * nshard
+    for rank in range(nshard):
+        start_id, end_id = num_per_shard*rank, num_per_shard*(rank+1)
+        fids_per_shard = fids[start_id: end_id]
+        future = pool.submit(detect_face_landmarks_per_shard, face_predictor_path, cnn_detector_path, input_dir, output_dir, fids_per_shard, rank)
+        future.add_done_callback(callback)
+        print(f"task {rank} submitted :{len(fids_per_shard)} files")
+    pool.shutdown()
+    unfinished_idx = [i for i, x in enumerate(finished) if x == 0]
+    print(*unfinished_idx)
+    if len(unfinished_idx) == 0:
+        print(f'ALL {sum(finished)} SHARDS ACCOMPLISHED.')
+    
+
+def detect_face_landmarks_per_shard(face_predictor_path, cnn_detector_path, input_dir, output_dir, fids_per_shard, rank):
+    detector = dlib.get_frontal_face_detector()
+    cnn_detector = dlib.cnn_face_detection_model_v1(cnn_detector_path)
+    predictor = dlib.shape_predictor(face_predictor_path)
+    
+    
+    for fid in tqdm(fids_per_shard):
         output_fn = os.path.join(output_dir, fid+'.pkl')
         video_path = os.path.join(input_dir, fid+'.mp4')
         frames = load_video(video_path)
@@ -50,7 +68,7 @@ def detect_face_landmarks(face_predictor_path, cnn_detector_path, root_dir, land
             landmarks.append(landmark)
         os.makedirs(os.path.dirname(output_fn), exist_ok=True)
         pickle.dump(landmarks, open(output_fn, 'wb'))
-    return
+    return rank
 
 
 if __name__ == '__main__':
@@ -59,14 +77,13 @@ if __name__ == '__main__':
     parser.add_argument('--root', type=str, help='root dir')
     parser.add_argument('--landmark', type=str, help='landmark dir')
     parser.add_argument('--manifest', type=str, help='a list of filenames')
-    parser.add_argument('--cnn_detector', type=str, help='path to cnn detector (download and unzip from: http://dlib.net/files/mmod_human_face_detector.dat.bz2)')
-    parser.add_argument('--face_predictor', type=str, help='path to face predictor (download and unzip from: http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)')
-    parser.add_argument('--rank', type=int, help='rank id')
-    parser.add_argument('--nshard', type=int, help='number of shards')
+    parser.add_argument('--cnn_detector', type=str, default='mmod_human_face_detector.dat', help='path to cnn detector (download and unzip from: http://dlib.net/files/mmod_human_face_detector.dat.bz2)')
+    parser.add_argument('--face_predictor', type=str, default='shape_predictor_68_face_landmarks.dat', help='path to face predictor (download and unzip from: http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)')
+    parser.add_argument('--nshard', type=int, default=1, help='number of shards')
     parser.add_argument('--ffmpeg', type=str, help='ffmpeg path')
     args = parser.parse_args()
     import skvideo
     skvideo.setFFmpegPath(os.path.dirname(args.ffmpeg))
     print(skvideo.getFFmpegPath())
     import skvideo.io
-    detect_face_landmarks(args.face_predictor, args.cnn_detector, args.root, args.landmark, args.manifest, args.rank, args.nshard)
+    detect_face_landmarks(args.face_predictor, args.cnn_detector, args.root, args.landmark, args.manifest, args.nshard)
